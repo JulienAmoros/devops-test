d08775f5f0300872dc9fdd41d6c0ebc2045d0a7b
Julien AMOROS <julien.amoros@cloud-temple.com>
2018-04-29 18:44:51 +0200

    upgrade to rails 5

--- app/views/users/show.json.jbuilder
+++ app/views/users/show.json.jbuilder
@@ -1,1 +1,1 @@
-json.extract! @user, :id, :email, :created_at, :updated_at
+json.partial! "users/user", user: @user
--- db/migrate/20180429163659_create_users.rb
+++ db/migrate/20180429163659_create_users.rb
@@ -1,9 +1,9 @@
-class CreateUsers < ActiveRecord::Migration
+class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
       t.string :email
 
-      t.timestamps null: false
+      t.timestamps
     end
   end
 end
